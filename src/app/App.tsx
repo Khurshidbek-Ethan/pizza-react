fd9faaac8a9be573769ea47e724547a36ca8d971
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-03-25 16:11:14 +0900

    fix: MUI cutomized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import { RippleBadge } from './materialTheme/MaterialTheme/styled';
 
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
         Create React App on TypeScript with REDUX
       </Typography>
     </Box>
-    <Button variant="contained">Contained</Button>
+    <Box>
+      <RippleBadge badgeContent = {4}>
+            <Button variant="contained" >Contained</Button>
+      </RippleBadge>
+    </Box>
     </Stack>
    </Container>
   );
