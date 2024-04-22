20407185cd437ca7eb1b5c6a7e1a219d2fc86652
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-04-22 12:44:48 +0900

    feat: retreive top users and modify ActiveUsers component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,11 +7,15 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import {setNewDishes, setPopularDishes} from "./slice";
+import {setNewDishes, setPopularDishes, setTopUsers} from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
 import "../../../css/home.css";
+
+
 
 
 
@@ -19,11 +23,12 @@ import "../../../css/home.css";
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
   setNewDishes: (data:Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
 
 
 export default function HomePage() {
-  const {setPopularDishes, setNewDishes} = actionDispatch(useDispatch());
+  const {setPopularDishes, setNewDishes, setTopUsers} = actionDispatch(useDispatch());
 
   
   
@@ -31,29 +36,34 @@ export default function HomePage() {
   useEffect(() => {
    // Backend serverdan data request => Data
    const product = new ProductService();
-   product.getProducts({
+   product
+   .getProducts({
     page: 1,
     limit: 4,
     order: "productViews",
     productCollection: ProductCollection.DISH,
-   }).then(data => {
-    // console.log("data passed here:",data);
-    setPopularDishes(data);
-   })
+   }).then(data => setPopularDishes(data))
    .catch((err) => console.log(err));
    
 
-    product.getProducts({
+    product
+    .getProducts({
     page: 1,
     limit: 4,
     order: "createdAt",
     productCollection: ProductCollection.DISH,
-   }).then(data => {
-    setNewDishes(data);
-   })
+   }).then(data => setNewDishes(data))
    .catch((err) => console.log(err));
 
    
+   const member = new MemberService();
+   member
+   .getTopUsers()
+   .then(data => setTopUsers(data))
+   .catch((err) => console.log(err));
+
+
+
 
   }, []);
 
