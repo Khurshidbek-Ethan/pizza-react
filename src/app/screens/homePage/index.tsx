aa8d27c6f6c5c7f73d8b1457e1d31ecae001fbac
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-04-22 10:30:55 +0900

    feat: develop product service business logic to retreive data from backend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,12 +7,12 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import {setPopularDishes} from "./slice";
-import {retrievePopularDishes} from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 
 /** REDUX SLICE & SELECTOR **/
@@ -20,28 +20,33 @@ const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data))
 });
 
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes, (popularDishes) => ({popularDishes}))
-  ;
-
 
 export default function HomePage() {
   const {setPopularDishes} = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
-  // console.log(process.env.REACT_APP_API_URL);
+
   
   
   
   useEffect(() => {
    // Backend serverdan data request => Data
- 
-
+   const product = new ProductService();
+   product.getProducts({
+    page: 1,
+    limit: 4,
+    order: "productViews",
+    productCollection: ProductCollection.DISH,
+   }).then(data => {
+    // console.log("data passed here:",data);
+    setPopularDishes(data);
+   })
+   .catch((err) => console.log(err));
+   
    // Slice: Data => Store
 
   }, []);
 
 
-  // console.log("popularDishes:",popularDishes);
+
   
 
 
@@ -56,3 +61,79 @@ export default function HomePage() {
    </div>
   );
 }
+
+
+
+
+// /////////////////////////////////////////////
+// 1chi shu yerdan boshla 
+// import React, { useEffect } from "react";
+// import Statistics from "./Statistics";
+// import PopularDishes from "./PopularDishes";
+// import NewDishes from "./NewDishes";
+// import Advertisement from "./Advertisement";
+// import ActiveUsers from "./ActiveUsers";
+// import Events from "./Events";
+// import "../../../css/home.css";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { Dispatch } from "@reduxjs/toolkit";
+// import { createSelector } from "reselect";
+// import {setPopularDishes} from "./slice";
+// import {retrievePopularDishes} from "./selector";
+// import { Product } from "../../../lib/types/product";
+// import ProductService from "../../services/ProductService";
+// import { ProductCollection } from "../../../lib/enums/product.enum";
+
+
+// /** REDUX SLICE & SELECTOR **/
+// const actionDispatch = (dispatch: Dispatch) => ({
+//   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data))
+// });
+
+// const popularDishesRetriever = createSelector(
+//   retrievePopularDishes, (popularDishes) => ({popularDishes})
+//  );
+
+
+// export default function HomePage() {
+//   const {setPopularDishes} = actionDispatch(useDispatch());
+//   const { popularDishes } = useSelector(popularDishesRetriever);
+//   // console.log(process.env.REACT_APP_API_URL);
+  
+  
+  
+//   useEffect(() => {
+//    // Backend serverdan data request => Data
+//    const product = new ProductService();
+//    product.getProducts({
+//     page: 1,
+//     limit: 4,
+//     order: "productViews",
+//     productCollection: ProductCollection.DISH,
+//    }).then(data => {
+//     // console.log("data passed here:",data);
+//     setPopularDishes(data);
+//    })
+//    .catch((err) => console.log(err));
+   
+//    // Slice: Data => Store
+
+//   }, []);
+
+
+//   console.log("popularDishes:",popularDishes);
+  
+
+
+//   return ( <div className={"homepage"}>
+//     <Statistics/>
+//     <PopularDishes/>
+//     <NewDishes/>
+//     <Advertisement/>
+//     <ActiveUsers/>
+//     <Events/>
+
+//    </div>
+//   );
+// }
