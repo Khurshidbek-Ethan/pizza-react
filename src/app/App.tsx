62a76767443cacd040e8cfb69bccd693f99318af
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-04-26 13:48:11 +0900

    feat: retreive data and modify products component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { Link, Route, Switch, useLocation } from "react-router-dom";
-import HomePage from "./screens/homePage";
-import ProductsPage from "./screens/userPage";
+
+import { Route, Switch, useLocation } from "react-router-dom";
+import ProductsPage from "./screens/productsPage";
 import OrdersPage from "./screens/ordersPage";
 import UserPage from "./screens/userPage";
+import HomePage from "./screens/homePage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
 import HelpPage from "./screens/helpPage";
+
+//import Test from "./screens/Test";
+
+// css imports
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import Test from "./screens/Test";
+
+/* A <Switch> looks through its children <Route>s and
+   renders the first one that matches the current URL. */
 
 function App() {
   const location = useLocation();
-  //  console.log("location:",location);
-
+  console.log("location:", location);
   return (
     <>
       {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
-
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -46,3 +50,51 @@ function App() {
 }
 
 export default App;
+
+// import React from "react";
+// import { Box, Button, Container, Stack, Typography } from "@mui/material";
+// import { Link, Route, Switch, useLocation } from "react-router-dom";
+// import HomePage from "./screens/homePage";
+// import ProductsPage from "./screens/userPage";
+// import OrdersPage from "./screens/ordersPage";
+// import UserPage from "./screens/userPage";
+// import HomeNavbar from "./components/headers/HomeNavbar";
+// import OtherNavbar from "./components/headers/OtherNavbar";
+// import Footer from "./components/footer";
+// import HelpPage from "./screens/helpPage";
+// import "../css/app.css";
+// import "../css/navbar.css";
+// import "../css/footer.css";
+// import Test from "./screens/Test";
+
+// function App() {
+//   const location = useLocation();
+//   //  console.log("location:",location);
+
+//   return (
+//     <>
+//       {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+
+//       <Switch>
+//         <Route path="/products">
+//           <ProductsPage />
+//         </Route>
+//         <Route path="/orders">
+//           <OrdersPage />
+//         </Route>
+//         <Route path="/member-page">
+//           <UserPage />
+//         </Route>
+//         <Route path="/help">
+//           <HelpPage />
+//         </Route>
+//         <Route path="/">
+//           <HomePage />
+//         </Route>
+//       </Switch>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
